b709a42d71337f046a8613d7bc4306b8220a6576
Coraline Thaon <coraline.thaon@gmail.com>
2023-09-15 19:11:47 +0200

    responsive menu burger avancé

--- wp-content/themes/foce-child/header.php
+++ wp-content/themes/foce-child/header.php
@@ -27,21 +27,22 @@
 
 	<header id="masthead" class="site-header">
 		<nav id="site-navigation" class="main-navigation">
-            <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
+            <!-- <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
                 <span class="line"></span>
                 <span class="line"></span>
                 <span class="line"></span>
-            </button>
+            </button> -->
             <ul>
                 <!-- <li><a href="#story">Histoire</a></li> -->
                 <!-- <li><a href="#characters">Personnages</a></li> -->
                 <li class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></li>
                 <!-- <li><a href="#place">Lieu</a></li> -->
                 <!-- <li><a href="#studio">Studio Koukaki</a></li> -->
-            </ul>
+            <!-- </ul> -->
 
         <!-- Integration du template menu burger -->
         <?php get_template_part( '/templates-parts/menu_burger' ); ?>
 
-		</nav><!-- #site-navigation -->
+		<!-- </nav> -->
+        <!-- #site-navigation -->
 	</header><!-- #masthead -->
